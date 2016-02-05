68ae08d7fb1cb6688d49a513f8a8d6eb0ea04349
Ceirra <ceilynschu@gmail.com>
2016-02-04 21:47:09 -0600

    trying

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("can I get this to git?");
+		
+		System.out.println("I am a clone!!");
 	}
 
 }
